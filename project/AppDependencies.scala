bf07c04bceff62d692b5fd05973ebbf7557f4484
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-11-22 13:35:40 +0000

    Add internal auth dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,8 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.33.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.12.0",
-    "uk.gov.hmrc"       %% "play-language"                  % "5.4.0-play-28"
+    "uk.gov.hmrc"       %% "play-language"                  % "5.4.0-play-28",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-28"   % "1.2.0"
   )
 
   val test = Seq(
