1b0db07ea07de2026cce30e4343f2bc581ae5f6f
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2023-04-17 16:47:57 +0100

    Update dependencies and swap to internal header

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.33.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.3.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.12.0",
-    "uk.gov.hmrc"       %% "play-language"                  % "5.4.0-play-28",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-28"   % "1.2.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.15.0",
+    "uk.gov.hmrc"       %% "play-language"                  % "6.2.0-play-28",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-28"   % "1.4.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.73.0"
   )
 
