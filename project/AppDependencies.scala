8231dd5bf2aa0e09742cab52aeaef1591268219e
Mike Burns <13621631+burns238@users.noreply.github.com>
2026-03-12 15:36:08 +0000

    Update versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,9 +7,9 @@ object AppDependencies {
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.10.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.12.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.32.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.5.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"           % "4.3.0",
     "org.typelevel"     %% "cats-core"                              % "2.8.0",
   )
